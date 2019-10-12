984335ebf4d7613796b134243cd51c3711bb1587
PoorviBhat <kpoorvibhat@gmail.com>
2019-10-12 16:30:41 +0530

    microservices

---
+++ zuul-gateway-service-proxy/src/test/java/com/stackroute/zuulgatewayserviceproxy/ZuulGatewayServiceProxyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stackroute.zuulgatewayserviceproxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZuulGatewayServiceProxyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
